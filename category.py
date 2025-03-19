456bf9ddced60ca5acfe64147d0065942ca3530e
h3brique <henriquebs012@gmail.com>
2025-03-19 11:09:31 -0300

    toString created category

--- category.py
+++ category.py
@@ -2,3 +2,6 @@ class Category:
     def __init__(self, name, description):
         self._name = name
         self._description = description
+
+        def __str__(self):
+            return f"Nome: {self._name} - Descrição: {self._description}"
